73bfcee0df3d647fbf9b343540b10a772618cb27
Diwash Lamichhane <imdiwash.mrprogrammer@gmail.com>
2022-01-06 11:45:33 +0545

    truncate algorithm edited

--- src/pages/reusable/truncate.js
+++ src/pages/reusable/truncate.js
@@ -1,6 +1,6 @@
 export function truncate(data, value) {
     if (data) {
-        var word = data.split(" ").splice(0, value + 1)
+        var word = data.split(" ").splice(0, value)
         return word.length < value ? data : word.join(" ") + '...'
     }
 }
