d1d8da0b23d6d837b6839a503433eb393d6cf2f3
Diwash Lamichhane <imdiwash.mrprogrammer@gmail.com>
2022-01-12 20:57:04 +0545

    added date

--- src/pages/publications/ResearchPapers.js
+++ src/pages/publications/ResearchPapers.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Link } from "react-router-dom";
 import { baseURL } from '../reusable/server';
 import { truncate } from '../reusable/truncate';
-import researchcover from"../../assests/researchpaper.jpg";
+import researchcover from "../../assests/researchpaper.jpg";
 
 
 export default class ResearchPapers extends Component {
@@ -93,7 +93,7 @@ export default class ResearchPapers extends Component {
                                                     <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="30" d="M128 48v32M384 48v32M464 160H48M304 260l43.42-32H352v168M191.87 306.63c9.11 0 25.79-4.28 36.72-15.47a37.9 37.9 0 0011.13-27.26c0-26.12-22.59-39.9-47.89-39.9-21.4 0-33.52 11.61-37.85 18.93M149 374.16c4.88 8.27 19.71 25.84 43.88 25.84 28.59 0 52.12-15.94 52.12-43.82 0-12.62-3.66-24-11.58-32.07-12.36-12.64-31.25-17.48-41.55-17.48"></path>
                                                 </svg>
                                             </span>
-                                            <span>{item.meta.title}</span>
+                                            <span>{item.date_published}</span>
                                         </div>
                                         <div className="d-flex align-items-center">
                                             <span className="me-2">
